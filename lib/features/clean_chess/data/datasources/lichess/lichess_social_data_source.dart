7fc3a8b0e47961f5ca033d87ba2359ce99b72dd8
riccardocescon <riccardo_cescon@libero.it>
2023-03-14 18:39:57 +0100

    - Created TDD for Social section

---
+++ lib/features/clean_chess/data/datasources/lichess/lichess_social_data_source.dart
@@ -0,0 +1,3 @@
+import 'package:cleanchess/features/clean_chess/data/datasources/remote_social_data_source.dart';
+
+class LichessSocialDataSource implements RemoteSocialDataSource {}
---
+++ lib/features/clean_chess/data/repositories/lichess/lichess_social_repository.dart
@@ -0,0 +1,3 @@
+import 'package:cleanchess/features/clean_chess/domain/repositories/social_repository.dart';
+
+class LichessSocialRepository implements SocialRepository {}
