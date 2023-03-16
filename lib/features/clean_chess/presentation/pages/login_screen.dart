aa200fe5cb8f31fc2899d82c6fb6b1db8d0fd9da
Alex Rintt <alexrintt@gmail.com>
2023-03-16 18:48:08 -0300

    Add base for theming through [ThemeData] class

--- lib/features/clean_chess/presentation/pages/login_screen.dart
+++ lib/features/clean_chess/presentation/pages/login_screen.dart
@@ -20,12 +20,13 @@ class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color(0xFF111111),
-      body: _listener(child: _body()),
+      body: _buildServerBlocListener(
+        child: _buildScaffoldBody(),
+      ),
     );
   }
 
-  Widget _body() => SizedBox(
+  Widget _buildScaffoldBody() => SizedBox(
         width: double.maxFinite,
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
@@ -33,7 +34,7 @@ class _LoginScreenState extends State<LoginScreen> {
             const AnimatedLinedBoard(),
             const SizedBox(height: 60),
             _lichessLoginButton(),
-            _continueAsGuestButton(),
+            _buildContinueAsGuestButton(),
           ],
         ),
       );
@@ -41,63 +42,47 @@ class _LoginScreenState extends State<LoginScreen> {
   Widget _lichessLoginButton() => SizedBox(
         width: _buttonSize.width,
         height: _buttonSize.height,
-        child: MaterialButton(
-          onPressed: () => BlocProvider.of<ServerBloc>(context).add(
-            const LichessOAuthEvent(),
+        child: ElevatedButton.icon(
+          onPressed: () {
+            BlocProvider.of<ServerBloc>(context).add(
+              const LichessOAuthEvent(),
+            );
+          },
+          icon: Image.asset(
+            'assets/img/lichess_logo.png',
+            color: Colors.black,
+            width: 30,
           ),
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(16),
-          ),
-          color: Colors.white,
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Image.asset(
-                'assets/img/lichess_logo.png',
-                color: Colors.black,
-                width: 30,
-              ),
-              const SizedBox(width: 20),
-              const Text(
-                'Login with Lichess',
-                style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 16,
-                  fontWeight: FontWeight.w400,
-                ),
-              ),
-            ],
-          ),
+          label: const Text('Login with Lichess'),
         ),
       );
 
-  Widget _continueAsGuestButton() => GestureDetector(
+  Widget _buildContinueAsGuestButton() => GestureDetector(
         onTap: () {},
         child: SizedBox(
           width: _buttonSize.width,
           height: _buttonSize.height,
-          child: Center(
+          child: const Center(
             child: Text(
               'Continue as guest',
-              style: TextStyle(
-                color: Colors.white.withAlpha(170),
-                fontSize: 16,
-                fontWeight: FontWeight.w400,
-              ),
               textAlign: TextAlign.center,
             ),
           ),
         ),
       );
 
-  Widget _listener({required Widget child}) =>
+  Widget _buildServerBlocListener({required Widget child}) =>
       BlocListener<ServerBloc, ServerState>(
         listener: (context, state) {
           if (state is LichessOAuthSuccess) {
             // Get user profile
-            Navigator.pushNamed(context, Navigation.homescreen);
+            if (context.mounted) {
+              Navigator.pushReplacementNamed(context, Navigation.homescreen);
+            }
           } else if (state is LichessError) {
-            showSnackbarError(context, state.failure);
+            if (context.mounted) {
+              showSnackbarError(context, state.failure);
+            }
           }
         },
         child: child,
--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,7 @@ import 'package:csv/csv.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_shared_tools/flutter_shared_tools.dart';
 
 final List<List<dynamic>> puzzleDb = [];
 
@@ -33,43 +34,272 @@ void main() async {
   log("Database loaded");
   puzzleDb.addAll(const CsvToListConverter().convert(myData));
 
-  runApp(
-    MultiBlocProvider(
+  runApp(const Root());
+}
+
+class Root extends StatefulWidget {
+  const Root({super.key});
+
+  @override
+  State<Root> createState() => _RootState();
+}
+
+class _RootState extends State<Root> {
+  @override
+  Widget build(BuildContext context) {
+    final ThemeData theme = createDefaultThemeData();
+
+    return GlobalProvider(
+      child: AnnotatedRegion<SystemUiOverlayStyle>(
+        value: SystemUiOverlayStyle(
+          statusBarColor: theme.scaffoldBackgroundColor,
+          systemNavigationBarColor: theme.scaffoldBackgroundColor,
+        ),
+        child: MaterialApp(
+          theme: theme,
+          debugShowCheckedModeBanner: false,
+          initialRoute: Navigation.loginScreen,
+          onGenerateRoute: (settings) {
+            switch (settings.name) {
+              case Navigation.loginScreen:
+                return PageRouteBuilder(
+                  settings: settings,
+                  pageBuilder: (context, animation, secondaryAnimation) =>
+                      const LoginScreen(),
+                );
+              case Navigation.homescreen:
+                return PageRouteBuilder(
+                  settings: settings,
+                  pageBuilder: (context, animation, secondaryAnimation) =>
+                      const HomeScreen(),
+                );
+              case Navigation.homepage:
+                return PageRouteBuilder(
+                  settings: settings,
+                  pageBuilder: (context, animation, secondaryAnimation) =>
+                      const Homepage(),
+                );
+              default:
+                return PageRouteBuilder(
+                  settings: settings,
+                  pageBuilder: (context, animation, secondaryAnimation) =>
+                      const LoginScreen(),
+                );
+            }
+          },
+        ),
+      ),
+    );
+  }
+}
+
+class GlobalProvider extends StatefulWidget {
+  const GlobalProvider({
+    super.key,
+    required this.child,
+  });
+
+  final Widget child;
+
+  @override
+  State<GlobalProvider> createState() => _GlobalProviderState();
+}
+
+class _GlobalProviderState extends State<GlobalProvider> {
+  @override
+  Widget build(BuildContext context) {
+    return MultiBlocProvider(
       providers: [
         BlocProvider<ServerBloc>(create: (context) => sl<ServerBloc>()),
       ],
-      child: MaterialApp(
-        initialRoute: Navigation.loginScreen,
-        onGenerateRoute: (settings) {
-          switch (settings.name) {
-            case Navigation.loginScreen:
-              return PageRouteBuilder(
-                settings: settings,
-                pageBuilder: (context, animation, secondaryAnimation) =>
-                    const LoginScreen(),
-              );
-            case Navigation.homescreen:
-              return PageRouteBuilder(
-                settings: settings,
-                pageBuilder: (context, animation, secondaryAnimation) =>
-                    const HomeScreen(),
-              );
-            case Navigation.homepage:
-              return PageRouteBuilder(
-                settings: settings,
-                pageBuilder: (context, animation, secondaryAnimation) =>
-                    const Homepage(),
-              );
-            default:
-              return PageRouteBuilder(
-                settings: settings,
-                pageBuilder: (context, animation, secondaryAnimation) =>
-                    const LoginScreen(),
-              );
-          }
-        },
-        theme: ThemeData.dark(),
+      child: widget.child,
+    );
+  }
+}
+
+ThemeData createDefaultThemeData() {
+  // TODO: Implement light and HUE based theme data functions.
+  return _createDarkThemeData();
+}
+
+ThemeData _createDarkThemeData() {
+  const Color kBackgroundColor = Color(0xff111111);
+  const Color kCardColor = kBackgroundColor;
+  const Color kCanvasColor = Color(0xff121212);
+  const Color kPrimaryColor = Color(0xFFFFFFFF);
+  const Color kSecondaryColor = Colors.white;
+
+  return createThemeData(
+    canvasColor: kCanvasColor,
+    backgroundColor: kBackgroundColor,
+    cardColor: kCardColor,
+    primaryColor: kPrimaryColor,
+    secondaryColor: kSecondaryColor,
+    textColor: Colors.white70,
+    headlineColor: Colors.white,
+    disabledColor: const Color.fromARGB(255, 78, 78, 78),
+    base: ThemeData.dark(),
+    fontFamily: AppFont.montserrat,
+  );
+}
+
+enum AppFont {
+  montserrat('Montserrat'),
+  robotoMono('Roboto Mono');
+
+  const AppFont(this.pubspecAssetKey);
+
+  final String pubspecAssetKey;
+}
+
+extension BrightnessInverse on Brightness {
+  Brightness get inverse =>
+      this == Brightness.dark ? Brightness.light : Brightness.dark;
+}
+
+ThemeData createThemeData({
+  required AppFont fontFamily,
+  required Color backgroundColor,
+  required Color cardColor,
+  required Color canvasColor,
+  required ThemeData base,
+  required Color secondaryColor,
+  required Color primaryColor,
+  required Color textColor,
+  required Color disabledColor,
+  required Color headlineColor,
+}) {
+  final String fontFamilyName = fontFamily.pubspecAssetKey;
+
+  final TextTheme textTheme =
+      base.textTheme.merge(Typography.material2021().black).apply(
+            fontFamily: fontFamilyName,
+            bodyColor: textColor,
+          );
+
+  base = base.copyWith(
+    splashColor: primaryColor.withOpacity(0.025),
+    highlightColor: primaryColor.withOpacity(0.025),
+  );
+
+  final OutlinedBorder defaultButtonShape = RoundedRectangleBorder(
+    borderRadius: BorderRadius.circular(16),
+  );
+
+  return base.copyWith(
+    bottomAppBarTheme: base.bottomAppBarTheme.copyWith(
+      elevation: 0,
+      color: backgroundColor,
+      surfaceTintColor: Colors.transparent,
+    ),
+    textButtonTheme: TextButtonThemeData(
+      style: (base.textButtonTheme.style ?? const ButtonStyle()).copyWith(
+        foregroundColor:
+            MaterialStateProperty.resolveWith<Color?>((_) => textColor),
+        surfaceTintColor: MaterialStateProperty.resolveWith<Color?>(
+          (_) => Colors.transparent,
+        ),
+        shape: MaterialStatePropertyAll<OutlinedBorder>(defaultButtonShape),
+        overlayColor: MaterialStateProperty.resolveWith<Color?>(
+          (Set<MaterialState> states) {
+            if (states.isNotEmpty) {
+              return canvasColor;
+            }
+            return null;
+          },
+        ),
       ),
     ),
+    scaffoldBackgroundColor: backgroundColor,
+    disabledColor: disabledColor,
+    textTheme: textTheme,
+    primaryColor: primaryColor,
+    dividerColor: disabledColor.withOpacity(.1),
+    appBarTheme: base.appBarTheme.copyWith(
+      surfaceTintColor: Colors.transparent,
+      backgroundColor: cardColor,
+      elevation: 0,
+      centerTitle: true,
+      titleTextStyle:
+          (base.appBarTheme.titleTextStyle ?? textTheme.displayLarge)!
+              .copyWith(color: textColor, fontSize: k8dp),
+      iconTheme: (base.appBarTheme.iconTheme ?? const IconThemeData.fallback())
+          .copyWith(
+        color: textColor,
+      ),
+      systemOverlayStyle: SystemUiOverlayStyle(
+        statusBarBrightness: base.brightness.inverse,
+        statusBarColor: Colors.transparent,
+      ),
+    ),
+    listTileTheme: base.listTileTheme.copyWith(
+      iconColor: textColor.withOpacity(0.5),
+      textColor: textColor,
+    ),
+    radioTheme: base.radioTheme.copyWith(
+      fillColor: MaterialStateProperty.all(primaryColor),
+    ),
+    dialogTheme: base.dialogTheme.copyWith(
+      surfaceTintColor: Colors.transparent,
+    ),
+    navigationBarTheme: base.navigationBarTheme.copyWith(
+      backgroundColor: backgroundColor,
+      elevation: 0,
+      height: kToolbarHeight * 1.3,
+      surfaceTintColor: Colors.transparent,
+      shadowColor: Colors.transparent,
+      indicatorColor: primaryColor,
+      labelTextStyle: MaterialStateProperty.resolveWith<TextStyle?>(
+        (Set<MaterialState> states) {
+          return textTheme.labelLarge!.copyWith(color: primaryColor);
+        },
+      ),
+      iconTheme: MaterialStateProperty.resolveWith<IconThemeData?>(
+        (Set<MaterialState> states) {
+          if (states.contains(MaterialState.selected)) {
+            return base.iconTheme.copyWith(color: backgroundColor);
+          }
+          return base.iconTheme.copyWith(color: primaryColor);
+        },
+      ),
+      labelBehavior: NavigationDestinationLabelBehavior.alwaysShow,
+    ),
+    elevatedButtonTheme: ElevatedButtonThemeData(
+      style: ButtonStyle(
+        overlayColor: MaterialStateProperty.resolveWith<Color?>(
+          (Set<MaterialState> states) {
+            if (states.isNotEmpty) {
+              return backgroundColor.withOpacity(.05);
+            }
+            return null;
+          },
+        ),
+        shape: MaterialStatePropertyAll<OutlinedBorder>(defaultButtonShape),
+        foregroundColor: MaterialStatePropertyAll(backgroundColor),
+        backgroundColor: MaterialStatePropertyAll(primaryColor),
+        textStyle: MaterialStateProperty.resolveWith<TextStyle?>(
+          (states) {
+            return textTheme.bodyLarge!.copyWith(color: backgroundColor);
+          },
+        ),
+      ),
+    ),
+    dialogBackgroundColor: backgroundColor,
+    canvasColor: canvasColor,
+    cardColor: cardColor,
+    tooltipTheme: base.tooltipTheme.copyWith(
+      textStyle: TextStyle(color: backgroundColor),
+      decoration: BoxDecoration(
+        color: textColor,
+        borderRadius: BorderRadius.circular(k1dp),
+      ),
+    ),
+    colorScheme: base.colorScheme.copyWith(
+      primary: primaryColor,
+      secondary: secondaryColor,
+      background: backgroundColor,
+      outline: disabledColor,
+    ),
+    useMaterial3: true,
   );
 }
