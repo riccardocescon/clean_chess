a6d1698a268e13e1793ad79a5fd3362ed22b8197
riccardocescon <riccardo_cescon@libero.it>
2023-03-21 21:24:38 +0100

    - Push to profile page

--- lib/features/clean_chess/presentation/widgets/homepage_appbar.dart
+++ lib/features/clean_chess/presentation/widgets/homepage_appbar.dart
@@ -1,5 +1,6 @@
 import 'package:auto_size_text/auto_size_text.dart';
 import 'package:cleanchess/core/clean_chess/utilities/style.dart';
+import 'package:cleanchess/features/clean_chess/presentation/pages/profile_screen.dart';
 import 'package:flutter/material.dart';
 
 class HomepageAppbar extends StatelessWidget {
@@ -42,7 +43,7 @@ class HomepageAppbar extends StatelessWidget {
         constraints:
             BoxConstraints(maxWidth: MediaQuery.of(context).size.width * 0.28),
         child: ElevatedButton(
-          onPressed: () {},
+          onPressed: () => _pushToProfilePage(context),
           style: ButtonStyle(
             backgroundColor: MaterialStateProperty.all<Color>(lightDark),
             elevation: MaterialStateProperty.all<double>(0),
@@ -69,6 +70,27 @@ class HomepageAppbar extends StatelessWidget {
                 ),
               ),
             ],
+          ),
+        ),
+      );
+
+  void _pushToProfilePage(BuildContext context) => Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => const ProfileScreen(
+            username: 'Logos',
+            memberSince: '21 February 2021',
+            followerNum: 7721,
+            onlineStatus: true,
+            fullName: 'Logos Ethos Pathos',
+            bio:
+                'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+            timeplayed: 100,
+            isFollowing: true,
+            userid: 'logos',
+            wonGames: 40,
+            lostGames: 30,
+            drawnGames: 30,
           ),
         ),
       );
