8ac552b1cb1716cd3e6517e38d3dff4d5504444a
hardal <ozdilpoyraz23@gmail.com>
2023-03-23 18:38:15 +0300

    Test commit

--- lib/features/clean_chess/presentation/pages/profile_screen.dart
+++ lib/features/clean_chess/presentation/pages/profile_screen.dart
@@ -1,7 +1,8 @@
-import 'package:cleanchess/features/clean_chess/presentation/pages/stats_screen.dart';
 import 'package:cleanchess/features/clean_chess/presentation/widgets/chessboard.dart';
 import 'package:flutter/material.dart';
 import 'package:cleanchess/features/clean_chess/presentation/widgets/profilepage_mode_items.dart';
+
+import '../widgets/chart.dart';
 
 class ProfileScreen extends StatelessWidget {
   const ProfileScreen({
@@ -521,6 +522,6 @@ void popupMenuButtonPressed() {}
 void navigateToStatsPage(BuildContext context) {
   Navigator.push(
     context,
-    MaterialPageRoute(builder: (context) => const StatsPage()),
+    MaterialPageRoute(builder: (context) => const Chart()),
   );
 }
