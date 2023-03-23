1030a32585fb056d30ab3a863c2c3781061a715b
hardal <ozdilpoyraz23@gmail.com>
2023-03-23 19:54:46 +0300

    Created the graph without side titles

--- lib/features/clean_chess/presentation/widgets/chart.dart
+++ lib/features/clean_chess/presentation/widgets/chart.dart
@@ -1,6 +1,11 @@
 import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
 
+int elo = 1300;
+int resultElo = 40; //How much elo the user gained/lost in a month
+int topELo = 2000; //The highest elo the user had this month
+int lowELo = 800; //The lowest elo the user had this month
+
 class Chart extends StatefulWidget {
   const Chart({super.key});
 
@@ -9,47 +14,114 @@ class Chart extends StatefulWidget {
 }
 
 class _ChartState extends State<Chart> {
+  List<Color> gradientColors = [
+    resultElo < 0 ? Colors.pink : Colors.green,
+    resultElo < 0 ? Colors.pink : Colors.green,
+  ];
+
+  bool showAvg = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView(
-        children: [
-          _chart(),
-        ],
+      body: Padding(
+        padding: const EdgeInsets.only(top: 40, left: 10, right: 10),
+        child: DefaultTextStyle(
+          style: const TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+          ),
+          child: SizedBox(
+            width: double.infinity,
+            height: 200,
+            child: LineChart(_mainData()),
+          ),
+        ),
       ),
     );
   }
-}
 
-Widget _chart() {
-  return Container(
-    padding: const EdgeInsets.all(10),
-    width: double.infinity,
-    height: 150,
-    child: LineChart(
-      LineChartData(
-        lineBarsData: [
-          LineChartBarData(
-              isCurved: true,
-              color: Colors.pink,
-              barWidth: 2,
-              dotData: FlDotData(show: false),
-              spots: [
-                const FlSpot(0, 1),
-                const FlSpot(1, 3),
-                const FlSpot(2, 10),
-                const FlSpot(3, 7),
-                const FlSpot(4, 12),
-                const FlSpot(5, 13),
-                const FlSpot(6, 17),
-                const FlSpot(7, 15),
-                const FlSpot(8, 12)
-              ]),
-        ],
-        borderData: FlBorderData(show: false),
-        gridData: FlGridData(show: false),
-        titlesData: FlTitlesData(show: false),
+  Widget topTitleWidgets() {
+    return Column(
+      children: [
+        Text("$elo"),
+        Text("$resultElo"),
+      ],
+    );
+  }
+
+  Widget bottomTitleWidgets() {
+    return const Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Text("21 April"),
+        Text("21 May"),
+      ],
+    );
+  }
+
+  Widget leftTitleWidgets() {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Text("$topELo"),
+        Text("$lowELo"),
+      ],
+    );
+  }
+
+  LineChartData _mainData() {
+    return LineChartData(
+      gridData: FlGridData(
+        show: false,
       ),
-    ),
-  );
+
+      titlesData: FlTitlesData(
+        show: false,
+      ),
+      borderData: FlBorderData(
+        show: true,
+      ),
+      //The X axis corresponds to the date, the Y axis to the elo
+      minX: 0,
+      maxX: 30,
+      minY: lowELo.toDouble(),
+      maxY: topELo.toDouble(),
+
+      lineBarsData: [
+        LineChartBarData(
+          spots: const [
+            FlSpot(0, 800),
+            FlSpot(3, 1200),
+            FlSpot(6, 1300),
+            FlSpot(9, 1200),
+            FlSpot(12, 1200),
+            FlSpot(15, 1500),
+            FlSpot(18, 1800),
+            FlSpot(21, 1200),
+            FlSpot(24, 1000),
+            FlSpot(27, 1600),
+            FlSpot(30, 1700),
+          ],
+          isCurved: true,
+          gradient: LinearGradient(
+            colors: gradientColors,
+          ),
+          barWidth: 2,
+          isStrokeCapRound: true,
+          dotData: FlDotData(
+            show: false,
+          ),
+          belowBarData: BarAreaData(
+            show: true,
+            gradient: LinearGradient(
+              colors: gradientColors
+                  .map((color) => color.withOpacity(0.3))
+                  .toList(),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
 }
