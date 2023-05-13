4d06c5d30824c792b5eea83ee395ffc75a202818
hardal <93533887+hardal7@users.noreply.github.com>
2023-05-13 22:35:02 +0300

    Revert "Revert "Set pieceTheme state on homepage""
    This reverts commit 312558a03907e0c48ab2c15425c0a0ae52c4eceb.

--- lib/features/clean_chess/presentation/widgets/board_piece.dart
+++ lib/features/clean_chess/presentation/widgets/board_piece.dart
@@ -2,6 +2,7 @@ import 'package:cleanchess/features/clean_chess/presentation/widgets/animated_bo
 import 'package:dartchess/dartchess.dart';
 import 'package:flutter/material.dart';
 import 'package:cleanchess/core/utilities/assets.dart' as assets;
+import 'package:flutter_svg/svg.dart';
 
 class BoardPiece extends StatelessWidget {
   const BoardPiece({
@@ -15,9 +16,11 @@ class BoardPiece extends StatelessWidget {
   final double scale;
   final PieceAnimation pieceAnim;
 
-  Widget get _piece => Image.asset(
-        assets.getPiecePath(piece),
-        scale: scale,
+  Widget get _piece => Transform.scale(
+        scale: scale / 10,
+        child: SvgPicture.asset(
+          assets.getPiecePath(piece),
+        ),
       );
 
   @override
